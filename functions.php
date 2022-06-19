8dc0798db8c5a0b65420dd3d56ad274ac1b79dcd
lynn <270146689@yoobeestudent.ac.nz>
2022-06-20 11:39:28 +1200

    prof page

--- functions.php
+++ functions.php
@@ -2,11 +2,21 @@
 
 //import css 
 define('CODE_BASE', get_template_directory_uri());
-function load_css()
+function salon_scripts()
 {
+    wp_enqueue_script('test', get_template_directory_uri() . '/js/test.js', array(), false, true);
+    wp_enqueue_script('masonry', get_template_directory_uri()  . '/js/macy.js', array(), false, true);
     wp_enqueue_style('main-style', CODE_BASE . '/css/style.css');
+    wp_enqueue_script('aos', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js', array(), null, true);
 }
-add_action('wp_enqueue_scripts', 'load_css');
+add_action('wp_enqueue_scripts', 'salon_scripts');
+
+function init_macy()
+{
+    wp_enqueue_script("macy-js", "https://cdn.jsdelivr.net/npm/macy@2");
+}
+
+add_action('wp_enqueue_scripts', 'init_macy');
 
 
 //menus
--- page-Portfolio.php
+++ page-Portfolio.php
@@ -1,16 +1,53 @@
-<?php get_header(); ?>
-<div class="container">
-
-    <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-    <div class="box">
-        <?php the_content() ?>
+<?php get_header("portfolio"); ?>
+<div class="port-page-wrap">
+    <div class="left">
+        <h1>What You Can Expect from Auckland Studio</h1>
+        <p>We offer exclusive and personalised men’s Barba services to all our customers. Our barbers are all highly
+            skilled to provide a first-class service. We offer a wide range of the finest products based on your skin
+            and hair type. Our experience has refined our services to provide the best in traditional barber services to
+            include traditional wet shaves, haircuts, bespoke skincare, and facial cleansing.
+            Our methods are traditional as they provide the finest and most unique techniques. Our barbers each have
+            specific skills to help you get what you desire. We don’t believe in sending clients out of our
+            establishment with something they don’t want. We understand that this is one of the most frustrating aspects
+            of trying a new barber and encourage our clients to bring in ideas, and we will provide professional advice.
+            Our barbers are up to date with the latest style trends so even if you don’t have an idea of what you might
+            want, we are here to help you discover your best-suited style.
+        </p>
     </div>
-    <?php endwhile; ?>
-    <?php endif; ?>
+    <div class="right"> <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <div class="container">
+            <?php the_content() ?>
+        </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+
 
 </div>
+<?php get_footer(); ?>
 
-<script src="minimasonry/build/minimasonry.min.js">
 
+<script>
+var masonry = new Macy({
+    container: '.container',
+    trueOrder: false,
+    waitForImages: false,
+    useOwnImageLoader: false,
+    debug: true,
+    mobileFirst: true,
+    columns: 1,
+    margin: {
+        y: 16,
+        x: '2%',
+    },
+    breakAt: {
+        1200: 6,
+        940: 5,
+        520: 3,
+        400: 2
+    },
+});
+
+console.log("minimasonry working")
 </script>
